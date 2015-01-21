a9630599096f07b1b1763d5ff5793b2d9d44792b
Stephen Lake <stephen.j.lake@gmail.com>
2015-01-21 08:32:37 +0000

    Fix up tests for all bells upto 30

--- src/test/java/com/concurrentperformance/ringingmaster/engine/notation/impl/NotationSplitterTest.java
+++ src/test/java/com/concurrentperformance/ringingmaster/engine/notation/impl/NotationSplitterTest.java
@@ -1,17 +1,15 @@
 package com.concurrentperformance.ringingmaster.engine.notation.impl;
 
-import static org.junit.Assert.assertArrayEquals;
-
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
 
-import com.concurrentperformance.ringingmaster.engine.notation.impl.NotationSplitter;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+import static org.junit.Assert.assertArrayEquals;
 
 @RunWith(Parameterized.class)
 public class NotationSplitterTest {
@@ -31,19 +29,19 @@ public class NotationSplitterTest {
 	@Parameters
 	public static Collection<Object[]> testValues() {
 		return Arrays.asList(new Object[][] { 
-			{ "X", new String[]{"X"} }, 
-			{ "x", new String[]{"x"} }, 
-			{ "x.X", new String[]{"x", "X"} }, 
-			{ ".x", new String[]{"x"} }, 
-			{ ".x.", new String[]{"x"} }, 
-			{ "ET", new String[]{"ET"} }, 
-			{ ".Et", new String[]{"Et"} }, 
-			{ ".Et.", new String[]{"Et"} }, 
-			{ "x.X.12", new String[]{"x", "X", "12"} }, 
-			{ "xX12", new String[]{"x", "X", "12"} }, 
-			{ "12.34.56.78.90.et.ET", new String[]{"12","34","56","78","90","et","ET"} }, 
-			{ "12X34X56X78X90XetXET", new String[]{"12","X","34","X","56","X","78","X","90","X","et","X","ET"} }, 
-			{ "x12zxl1l2", new String[]{"x","12","x","1","2"} }, 
+			{ "X", new String[]{"X"} },
+			{ "x", new String[]{"x"} },
+			{ "x.X", new String[]{"x", "X"} },
+			{ ".x", new String[]{"x"} },
+			{ ".x.", new String[]{"x"} },
+			{ "ET", new String[]{"ET"} },
+			{ ".Et", new String[]{"Et"} },
+			{ ".Et.", new String[]{"Et"} },
+			{ "x.X.12", new String[]{"x", "X", "12"} },
+			{ "xX12", new String[]{"x", "X", "12"} },
+			{ "12.34.56.78.90.et.ET", new String[]{"12","34","56","78","90","et","ET"} },
+			{ "12X34X56X78X90XetXET", new String[]{"12","X","34","X","56","X","78","X","90","X","et","X","ET"} },
+			{ "x12zxz1z2", new String[]{"x","12","x","1","2"} },
 			});
 	}
 
