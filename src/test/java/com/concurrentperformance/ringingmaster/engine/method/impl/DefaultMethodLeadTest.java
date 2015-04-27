e32938004646a5e9670d4b687f730c8e78e93a86
Stephen Lake <stephen.j.lake@gmail.com>
2015-04-27 09:24:31 +0200

    tidy up the generation of the arrays.

--- src/test/java/com/concurrentperformance/ringingmaster/engine/method/impl/DefaultMethodLeadTest.java
+++ src/test/java/com/concurrentperformance/ringingmaster/engine/method/impl/DefaultMethodLeadTest.java
@@ -30,10 +30,10 @@ public class DefaultMethodLeadTest {
 
 		final MethodLead lead = new DefaultMethodLead(NumberOfBells.BELLS_4, rows, empty);
 
-		assertEquals(buildIntegerList(0,1,2,3), lead.getPlaceSequenceForBell(Bell.BELL_1));
-		assertEquals(buildIntegerList(1,0,0,1), lead.getPlaceSequenceForBell(Bell.BELL_2));
-		assertEquals(buildIntegerList(2,3,3,2), lead.getPlaceSequenceForBell(Bell.BELL_3));
-		assertEquals(buildIntegerList(3,2,1,0), lead.getPlaceSequenceForBell(Bell.BELL_4));
+		assertEquals(Arrays.asList(0, 1, 2, 3), lead.getPlaceSequenceForBell(Bell.BELL_1));
+		assertEquals(Arrays.asList(1, 0, 0, 1), lead.getPlaceSequenceForBell(Bell.BELL_2));
+		assertEquals(Arrays.asList(2, 3, 3, 2), lead.getPlaceSequenceForBell(Bell.BELL_3));
+		assertEquals(Arrays.asList(3, 2, 1, 0), lead.getPlaceSequenceForBell(Bell.BELL_4));
 
 	}
 
@@ -56,13 +56,9 @@ public class DefaultMethodLeadTest {
 
 	@Test(expected=IllegalArgumentException.class)
 	public void cantConstructLeadWithEmptyRows() {
-		final MethodLead lead = new DefaultMethodLead(NumberOfBells.BELLS_8, new ArrayList<MethodRow>(), empty);
+		final MethodLead lead = new DefaultMethodLead(NumberOfBells.BELLS_8, new ArrayList<>(), empty);
 
 		assertNull(lead.getLastRow().getDisplayString(false));
-	}
-
-	private List<Integer> buildIntegerList(final Integer... integers) {
-		return Arrays.asList(integers);
 	}
 
 	private MethodRow buildRow(final NumberOfBells numberOfBells, final Bell... bells) {
