f948f40e69ddedad17cce9afe292391330caf058
Stephen Lake <stephen.j.lake@gmail.com>
2015-05-07 08:35:23 +0100

    Tidy up the main lead head test

--- src/test/java/com/concurrentperformance/ringingmaster/engine/notation/impl/CheckAllLeadHeadsTest.java
+++ src/test/java/com/concurrentperformance/ringingmaster/engine/notation/impl/CheckAllLeadHeadsTest.java
@@ -2,6 +2,7 @@ package com.concurrentperformance.ringingmaster.engine.notation.impl;
 
 
 import com.concurrentperformance.ringingmaster.engine.NumberOfBells;
+import com.concurrentperformance.ringingmaster.engine.helper.PlainCourseHelper;
 import com.concurrentperformance.ringingmaster.engine.notation.NotationBody;
 import com.concurrentperformance.ringingmaster.generated.notation.persist.SerializableNotation;
 import com.ringingmaster.extraction.CentralCouncilMethodExtractor;
@@ -15,7 +16,6 @@ import java.util.Collection;
 import java.util.stream.Collectors;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
 
 @RunWith(Parameterized.class)
 public class CheckAllLeadHeadsTest {
@@ -27,8 +27,8 @@ public class CheckAllLeadHeadsTest {
 	public static Collection<Object[]> checkAllCCLibrary() {
 		return new CentralCouncilMethodExtractor()
 				.extractNotationsToStream()
-//				.filter(serializableNotation -> serializableNotation.getNumberOfBells() == 8)
-//				.filter(serializableNotation -> serializableNotation.getName().contains("Waterloo Reverse Bob"))
+				.filter(serializableNotation -> serializableNotation.getNumberOfBells() == 8)
+				.filter(serializableNotation -> serializableNotation.getName().contains("Waterloo Reverse Bob"))
 				.map(serializableNotation -> new Object[]{serializableNotation})
 				.collect(Collectors.toList());
 	}
@@ -53,24 +53,16 @@ public class CheckAllLeadHeadsTest {
 
 
 		// Uncomment section to log out the changes in the lead
-//		log.warn(notationBody.toString());
-//		log.warn(PlainCourseHelper.buildPlainCourse(notationBody, "TEST", false).getCreatedMethod().getLead(0).toString());
+		log.warn(notationBody.toString());
+		log.warn(PlainCourseHelper.buildPlainCourse(notationBody, "TEST", false).getCreatedMethod().getLead(0).toString());
 
 		LeadHeadCalculator.LeadHeadCodeType leadHeadType = LeadHeadCalculator.getLeadHeadType(ccLeadHead, ccNumberOfBells);
 
-		String calculatedLeadHead = null;
+		String calculatedLeadHead = notationBody.getLeadHeadCode();
 		if (leadHeadType == LeadHeadCalculator.LeadHeadCodeType.VALID_LEADHEAD_ROW) {
-
 			//TODO need to exclude non plain bob lead heads from calculating a code.
 			// These rows are where it is not a plain bob lead head, and therefore no code is in the cc library.
-//			calculatedLeadHead = LeadHeadCalculator.lookupRowFromCode(notationBody.getLeadHeadCode(), notationBody.getNumberOfWorkingBells());
-			calculatedLeadHead = notationBody.getLeadHeadCode();
-		}
-		else if (leadHeadType == LeadHeadCalculator.LeadHeadCodeType.VALID_LEADHEAD_CODE){
-			calculatedLeadHead = notationBody.getLeadHeadCode();
-		}
-		else {
-			fail();
+			calculatedLeadHead = LeadHeadCalculator.lookupRowFromCode(notationBody.getLeadHeadCode(), notationBody.getNumberOfWorkingBells());
 		}
 
 		assertEquals("[" + notationBody.getNumberOfWorkingBells().getBellCount() + "] " + notationBody.getNameIncludingNumberOfBells() +
