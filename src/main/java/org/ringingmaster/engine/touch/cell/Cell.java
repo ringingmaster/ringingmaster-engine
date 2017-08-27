34ae016e48bbd505c8a4492d274c6cf61e36a780
Stephen Lake <stephen.j.lake@gmail.com>
2017-08-27 11:56:03 +0100

    add immutable cell classes

---
+++ src/main/java/org/ringingmaster/engine/touch/cell/Cell.java
@@ -0,0 +1,16 @@
+package org.ringingmaster.engine.touch.cell;
+
+import org.ringingmaster.engine.touch.container.TouchElement;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * TODO Comments
+ *
+ * @author Lake
+ */
+public class Cell {
+
+    private List<TouchElement> elements = new ArrayList<>();
+}
